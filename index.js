97ad277445c0fecbe8258ed0858fa6487dbb3f6f
Traci Chan <tlc668@gmail.com>
2023-05-15 15:50:31 -0400

    added random button

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ function paint(color) {
     const circle =document.getElementById('circleID');
     circle.style = `background-color:${color}`;
     console.log(circle);
+    if (color == 'random'){
+        console.log('in random')
+        
+        circle.style = background-color;"randomColor";
+    }
+}
+function randomColor(){
+    var randomColor = '#' + (Math.floor(Math.random()*16777215).toString(16));
+    console.log(randomColor);
+    const circle = document.getElementById('circleID').style.backgroundColor = randomColor;
+    
 }
