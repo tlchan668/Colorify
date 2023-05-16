63fe90eded1db2c5a8e898ab2283ca8a5ea92b27
Traci Chan <tlc668@gmail.com>
2023-05-16 13:35:06 -0400

    clean up code

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ function paint(color) {
     console.log(color);
     const circle =document.getElementById('circleID');
     circle.style = `background-color:${color}`;
-    console.log(circle);
-    if (color == 'random'){
-        console.log('in random')
-        
-        circle.style = background-color;"randomColor";
-    }
+    
 }
 function randomColor(){
     var randomColor = '#' + (Math.floor(Math.random()*16777215).toString(16));
